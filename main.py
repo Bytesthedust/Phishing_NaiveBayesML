e9838b8f2b6da03ffa0dab3a095fbba0443293a2
Bytesthedust <47130778+Bytesthedust@users.noreply.github.com>
2023-12-05 22:37:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ NX_all = model.transform(features)
 print(NX_all.shape)
 
 # NAIVE BAYES TRAINING
+# Model training
 
 # splitting dataset
 NX_train, NX_test, NY_train, NY_test = train_test_split(NX_all, labels, test_size = 0.2, random_state = 42)
@@ -63,26 +64,37 @@ NX_train, NX_test, NY_train, NY_test = train_test_split(NX_all, labels, test_siz
 nbModel = MultinomialNB(force_alpha=True).fit(NX_train, NY_train.ravel())
 NY_pred = nbModel.predict(NX_test)
 
-# construct and display confusion matrix
+# construct confusion matrix
 tn, fp, fn, tp = confusion_matrix(NY_test,NY_pred).ravel()
-cmatrix = ConfusionMatrixDisplay.from_predictions(NY_test, NY_pred)
 
 # matrix metrics
 confusion_matrix_metrics(tp, fp, tn, fn)
 
-# Display ROC curve
+# ROC curve calculation
 fpr, tpr, thresholds = roc_curve(NY_test, NY_pred)
 roc_auc = roc_auc_score(NY_test, NY_pred)
 
-plt.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], 'k--')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Naive Bayes ROC Curve')
-plt.legend()
+# Display Plots
+
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+# Confusion Matrix subplot
+cmatrix = ConfusionMatrixDisplay.from_predictions(NY_test, NY_pred, ax=ax1)
+ax1.set_title('Naive Bayes Confusion Matrix')
+
+# ROC Curve subplot
+ax2.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
+ax2.plot([0, 1], [0, 1], 'k--')
+ax2.set_xlabel('False Positive Rate')
+ax2.set_ylabel('True Positive Rate')
+ax2.set_title('Naive Bayes ROC Curve')
+ax2.legend()
+
+plt.show()
 
 # BOOTSTRAPPING
-n_bootstraps = 1050
+n_bootstrap = 1050
 
 bootstrap_stats = []
 
@@ -95,7 +107,7 @@ for _ in range(n_bootstrap):
   bootstrap_stats.append(bootstrap_features.mean(axis=0))
 
 # running features through decision tree to determine relevant features
-decisionModel = DecisionTreeClassifier()
+decisionModel = DecisionTreeClassifier() #bootstrap depth = 13
 decisionModel = decisionModel.fit(bootstrap_features, bootstrap_labels)
 importance = decisionModel.feature_importances_
 
@@ -116,6 +128,7 @@ model = SelectFromModel(decisionModel, prefit=True)
 BX_all = model.transform(bootstrap_features)
 print(BX_all.shape)
 
+# BOOTSTRAP TRAINING
 # retraining model
 BX_train, BX_test, BY_train, BY_test = train_test_split(bootstrap_features, bootstrap_labels, test_size = 0.2, random_state = 42)
 
@@ -125,32 +138,43 @@ BY_pred = nbModel.predict(BX_test)
 
 # construct and display confusion matrix
 tn, fp, fn, tp = confusion_matrix(BY_test,BY_pred).ravel()
-cmatrix = ConfusionMatrixDisplay.from_predictions(BY_test, BY_pred)
 
 # matrix metrics
 confusion_matrix_metrics(tp, fp, tn, fn)
 
-# ROC curve
+# ROC curve calculation
 fpr, tpr, thresholds = roc_curve(BY_test, BY_pred)
 roc_auc = roc_auc_score(BY_test, BY_pred)
 
-plt.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], 'k--')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Bootstrap ROC Curve')
-plt.legend()
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+# Confusion Matrix subplot
+cmatrix = ConfusionMatrixDisplay.from_predictions(NY_test, NY_pred, ax=ax1)
+ax1.set_title('Bootstrap Confusion Matrix')
+fpr, tpr, thresholds = roc_curve(BY_test, BY_pred)
+roc_auc = roc_auc_score(BY_test, BY_pred)
+# ROC Curve subplot
+ax2.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
+ax2.plot([0, 1], [0, 1], 'k--')
+ax2.set_xlabel('False Positive Rate')
+ax2.set_ylabel('True Positive Rate')
+ax2.set_title('Naive Bayes ROC Curve')
+ax2.legend()
+
+plt.show()
 
 # BAGGING
+num_models = 1050
 model_preds = []
 
-for _ in range(n_bootstraps):
+for _ in range(num_models):
   BGX_train, BGX_test, BGY_train, BGY_test = train_test_split(bootstrap_features, bootstrap_labels, test_size = 0.2, random_state = 42, stratify=labels)
   nbModel = MultinomialNB(force_alpha=True).fit(BGX_train, BGY_train.ravel())
   model_preds.append(nbModel.predict(BGX_test))
 
 
-ensemble_predictions = np.mean(model_preds, axis=0) # aggregating predicitions
+ensemble_predictions = np.mean(model_preds, axis=0)  # You can also use np.median() for voting
 
 # Evaluate the ensemble model's metrics
 ensemble_accuracy = np.mean(ensemble_predictions == BGY_test)
@@ -161,9 +185,6 @@ print("Ensemble Model Accuracy:", ensemble_accuracy)
 confusion_matrix_metrics(tp, fp, tn, fn,True)
 
 # Ada Boosting
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.metrics import accuracy_score
-
 AX_train, AX_test, AY_train, AY_test = train_test_split(NX_all, labels, test_size = 0.2, random_state = 42)
 
 base_model = DecisionTreeClassifier(max_depth=1)
@@ -174,25 +195,34 @@ AY_pred = adaboost_classifier.predict(AX_test)
 
 # construct and display confusion matrix
 tn, fp, fn, tp = confusion_matrix(AY_test,AY_pred).ravel()
-cmatrix = ConfusionMatrixDisplay.from_predictions(AY_test, AY_pred)
 
 # matrix metrics
 confusion_matrix_metrics(tp, fp, tn, fn)
 
-# ROC curve
+# ROC curve calculation
 fpr, tpr, thresholds = roc_curve(AY_test, AY_pred)
 roc_auc = roc_auc_score(AY_test, AY_pred)
 
-plt.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], 'k--')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Ada Boosting ROC Curve')
-plt.legend()
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+# Confusion Matrix subplot
+cmatrix = ConfusionMatrixDisplay.from_predictions(NY_test, NY_pred, ax=ax1)
+ax1.set_title('Ada Confusion Matrix')
+fpr, tpr, thresholds = roc_curve(BY_test, BY_pred)
+roc_auc = roc_auc_score(BY_test, BY_pred)
+# ROC Curve subplot
+ax2.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
+ax2.plot([0, 1], [0, 1], 'k--')
+ax2.set_xlabel('False Positive Rate')
+ax2.set_ylabel('True Positive Rate')
+ax2.set_title('Naive Bayes ROC Curve')
+ax2.legend()
+
+plt.show()
 
 # CROSS-VALIDATION
 # Naive Bayes
-
 train_scores, test_scores = list(), list()
 # define the tree depths to evaluate
 values = [i for i in range(1, 21)]
@@ -215,51 +245,107 @@ for i in values:
  # summarize progress
  print('>%d, train: %.3f, test: %.3f' % (i, train_acc, test_acc))
 
+# K-Folds cross validation
+# define the tree depths to evaluate
+values = [i for i in range(1, 21)]
+
+depths = []
+mean_scores = []
+# evaluate a decision tree for each depth
+for i in values:
+ # configure the model
+ model = DecisionTreeClassifier(max_depth=i)
+ # Perform 7-fold cross validation
+ scores = cross_val_score(estimator=model, X=features, y=labels, cv=7, n_jobs=4)
+ mean_scores.append(scores.mean())
+ depths.append((i,scores.mean()))
+
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
 
 # plot of train and test scores vs tree depth
-plt.plot(values, train_scores, '-o', label='Train')
-plt.plot(values, test_scores, '-o', label='Test')
-plt.title('Naive Bayes Cross Validation')
-plt.xlabel("Values")
-plt.ylabel("Scores")
-plt.legend()
+ax1.plot(values, mean_scores, '-o')
+ax1.set_title('Naive Bayes Cross Validation')
+ax1.set_xlabel("Mean Values")
+ax1.set_ylabel("Scores")
+
+# plot of train and test scores vs tree depth
+ax2.plot(values, train_scores, '-o', label='Train')
+ax2.plot(values, test_scores, '-o', label='Test')
+ax2.set_title('Naive Bayes Cross Validation')
+ax2.set_xlabel("Values")
+ax2.set_ylabel("Scores")
+ax2.legend()
+
+
 plt.show()
 
-# Bootstrap
 
+# Bootstrap
 train_scores, test_scores = list(), list()
 
-#define the tree depths to evaluate
+# define the tree depths to evaluate
 values = [i for i in range(1, 21)]
 
 
-#evaluate a decision tree for each depth
+# evaluate a decision tree for each depth
 for i in values:
- #configure the model
+ # configure the model
  model = DecisionTreeClassifier(max_depth=i)
- #fit model on the training dataset
+ # fit model on the training dataset
  model.fit(BX_train, BY_train)
- #evaluate on the train dataset
+ # evaluate on the train dataset
  train_yhat = model.predict(BX_train)
  train_acc = accuracy_score(BY_train, train_yhat)
  train_scores.append(train_acc)
- #evaluate on the test dataset
+ # evaluate on the test dataset
  test_yhat = model.predict(BX_test)
  test_acc = accuracy_score(BY_test, test_yhat)
  test_scores.append(test_acc)
- #summarize progress
+ # summarize progress
  print('>%d, train: %.3f, test: %.3f' % (i, train_acc, test_acc))
 
-#plot of train and test scores vs tree depth
-plt.plot(values, train_scores, '-o', label='Train')
-plt.plot(values, test_scores, '-o', label='Test')
-plt.title('Bootstrap Cross Validation')
-plt.xlabel("Values")
-plt.ylabel("Scores")
-plt.legend()
+# K folds booststrap
+# define the tree depths to evaluate
+values = [i for i in range(1, 21)]
+
+depths = []
+mean_scores = []
+# evaluate a decision tree for each depth
+for i in values:
+ # configure the model
+ model = DecisionTreeClassifier(max_depth=i)
+ # Perform 7-fold cross validation
+ scores = cross_val_score(estimator=model, X=bootstrap_features, y=bootstrap_labels, cv=7, n_jobs=4)
+ mean_scores.append(scores.mean())
+ depths.append((i,scores.mean()))
+
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+
+# plot of train and test scores vs tree depth
+ax1.plot(values, mean_scores, '-o')
+ax1.set_title('Bootstrap Cross Validation')
+ax1.set_xlabel("Mean Values")
+ax1.set_ylabel("Scores")
+
+
+
+# plot of train and test scores vs tree depth
+ax2.plot(values, train_scores, '-o', label='Train')
+ax2.plot(values, test_scores, '-o', label='Test')
+ax2.set_title('Bootstrap Cross Validation')
+ax2.set_xlabel("Values")
+ax2.set_ylabel("Scores")
+ax2.legend()
+
+
 plt.show()
 
-# Cross Validated NB
+
+# CROSS VALIDATED NAIVE BAYES
 # Feature Engineering
 
 #running features through decision tree to determine relevant features
@@ -285,32 +371,44 @@ model = SelectFromModel(decisionModel, prefit=True)
 NX_all = model.transform(features)
 print(NX_all.shape)
 
+# CROSS VALIDATED NAIVE BAYES TRAINING AND PLOT
 # splitting dataset
 NX_train, NX_test, NY_train, NY_test = train_test_split(NX_all, labels, test_size = 0.2, random_state = 42)
 
-#training the model
+# training the model
 nbModel = MultinomialNB(force_alpha=True).fit(NX_train, NY_train.ravel())
 NY_pred = nbModel.predict(NX_test)
 
-#construct and display confusion matrix
+# construct and display confusion matrix
 tn, fp, fn, tp = confusion_matrix(NY_test,NY_pred).ravel()
-cmatrix = ConfusionMatrixDisplay.from_predictions(NY_test, NY_pred)
 
-#matrix metrics
+# matrix metrics
 confusion_matrix_metrics(tp, fp, tn, fn)
 
-#Display ROC curve
+# ROC curve calculation
 fpr, tpr, thresholds = roc_curve(NY_test, NY_pred)
 roc_auc = roc_auc_score(NY_test, NY_pred)
 
-plt.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], 'k--')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Cross Validation Naives Bayes ROC Curve')
-plt.legend()
+# Display Plots
 
-# Cross Validated Bootstrap
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+# Confusion Matrix subplot
+cmatrix = ConfusionMatrixDisplay.from_predictions(NY_test, NY_pred, ax=ax1)
+ax1.set_title('Cross Validated Naive Bayes Confusion Matrix')
+
+# ROC Curve subplot
+ax2.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
+ax2.plot([0, 1], [0, 1], 'k--')
+ax2.set_xlabel('False Positive Rate')
+ax2.set_ylabel('True Positive Rate')
+ax2.set_title('Cross Validated Naive Bayes ROC Curve')
+ax2.legend()
+
+plt.show()
+
+# CROSS VALIDATED BOOTSTRAP
 
 # running features through decision tree to determine relevant features
 decisionModel = DecisionTreeClassifier(max_depth=13) #bootstrap depth = 13
@@ -334,6 +432,7 @@ BX_all = model.transform(bootstrap_features)
 print(BX_all.shape)
 
 
+# CROSS VALIDATED BOOTSTRAP TRAINING
 # retraining model
 BX_train, BX_test, BY_train, BY_test = train_test_split(bootstrap_features, bootstrap_labels, test_size = 0.2, random_state = 42)
 
@@ -343,18 +442,28 @@ BY_pred = nbModel.predict(BX_test)
 
 # construct and display confusion matrix
 tn, fp, fn, tp = confusion_matrix(BY_test,BY_pred).ravel()
-cmatrix = ConfusionMatrixDisplay.from_predictions(BY_test, BY_pred)
 
 # matrix metrics
 confusion_matrix_metrics(tp, fp, tn, fn)
 
-# ROC curve
+# ROC curve calculation
 fpr, tpr, thresholds = roc_curve(BY_test, BY_pred)
 roc_auc = roc_auc_score(BY_test, BY_pred)
 
-plt.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], 'k--')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Cross Validation Bootstrap ROC Curve')
-plt.legend()
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+# Confusion Matrix subplot
+cmatrix = ConfusionMatrixDisplay.from_predictions(NY_test, NY_pred, ax=ax1)
+ax1.set_title(' CV Bootstrap Confusion Matrix')
+fpr, tpr, thresholds = roc_curve(BY_test, BY_pred)
+roc_auc = roc_auc_score(BY_test, BY_pred)
+# ROC Curve subplot
+ax2.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
+ax2.plot([0, 1], [0, 1], 'k--')
+ax2.set_xlabel('False Positive Rate')
+ax2.set_ylabel('True Positive Rate')
+ax2.set_title('CV Bootstrap ROC Curve')
+ax2.legend()
+
+plt.show()
